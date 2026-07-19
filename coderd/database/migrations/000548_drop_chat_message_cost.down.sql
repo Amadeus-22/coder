88e6345344799f465d81e74d5870d26cb7314bb1
Michael Suchacz <203725896+ibetitsmike@users.noreply.github.com>
2026-07-19 01:41:51 +0000

    fix: leave spend index rollback to migration 000548

--- coderd/database/migrations/000548_drop_chat_message_cost.down.sql
+++ coderd/database/migrations/000548_drop_chat_message_cost.down.sql
@@ -1,5 +1,1 @@
 ALTER TABLE chat_messages ADD COLUMN total_cost_micros BIGINT;
-
-CREATE INDEX idx_chat_messages_owner_spend
-    ON chat_messages (chat_id, created_at)
-    WHERE total_cost_micros IS NOT NULL;
--- coderd/database/migrations/000548_drop_chat_message_cost.up.sql
+++ coderd/database/migrations/000548_drop_chat_message_cost.up.sql
@@ -1,3 +1,1 @@
-DROP INDEX IF EXISTS idx_chat_messages_owner_spend;
-
 ALTER TABLE chat_messages DROP COLUMN total_cost_micros;
